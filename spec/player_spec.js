d4c2b00bd2c0e061307188266dc91366c699c66c
jtong <tj19832@gmail.com>
2015-01-10 21:28:45 +0800

    jtong - add jsmockito info and example

---
+++ spec/player_spec.js
@@ -0,0 +1,11 @@
+var m = require('jsmockito').JsMockito;
+//var Player = require('../src/player.js');
+// about jsmockito : https://github.com/cleishm/jsmockito
+
+describe("player", function(){
+    it("game spec", function(){
+        var mocked_console = m.spy(console);
+        console.log("李四被打败了");
+        m.verify(mocked_console).log("李四被打败了.");
+    });
+});
