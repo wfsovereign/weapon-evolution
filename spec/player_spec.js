aa13acc040515f9eb6a2110e2f7f6501110be852
wfsovereign <never_fall@sohu.com>
2015-01-14 13:35:26 +0800

    success test 1、2

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -5,10 +5,10 @@ var player = require ('../src/Player.js');
 //var Player = require('../src/player.js');
 // about jsmockito : https://github.com/cleishm/jsmockito
 
-xdescribe("fight", function(){
+describe("fight", function(){
     it("should output 李四被打败了", function(){
         var zhangs = new player("张三",300,8);
-        var lis = new   player("李四",20,9);
+        var lis = new player("李四",20,9);
         var console_fake = {
             info:'',
             log:function(text){
@@ -16,11 +16,11 @@ xdescribe("fight", function(){
             }
         };
         fight(zhangs,lis,console_fake);
-      expect(console_fake.info).toEqual("张三被打败了.")
+      expect(console_fake.info).toEqual("李四被打败了.")
     });
     it("should output 张三被打败了", function(){
         var zhangs = new player("张三",10,8);
-        var lis = new   player("李四",20,9);
+        var lis = new player("李四",20,9);
         var console_fake = {
             info:'',
             log:function(text){
@@ -30,7 +30,7 @@ xdescribe("fight", function(){
         //var mocked_console = m.spy(console);
         fight(zhangs,lis,console_fake);
 
-        m.verify(console_fake).log("张三被打败了.");
+       expect(console_fake.info).toEqual("张三被打败了.")
     });
 
 });
@@ -47,7 +47,7 @@ describe("2", function(){
         "李四攻击了张三,张三受到了9点伤害,张三剩余生命：1\n"+
         "张三攻击了李四,李四受到了8点伤害,李四剩余生命：4\n"+
         "李四攻击了张三,张三受到了9点伤害,张三剩余生命：-8\n"+
-        "张三被打败了";
+        "张三被打败了.";
         var zhangs = new player("张三",10,8);
         var lis = new   player("李四",20,9);
         //var mocked_console = m.spy(console);
--- src/Fight.js
+++ src/Fight.js
@@ -3,22 +3,22 @@
  */
 
 function Fight(player1,player2,console){
-    var result="";
+    //var result="";
     while (player1.HP > 0 && player2.HP > 0){
         //player2.HP -= player1.AP;
-        result += player1.attack(player2);
+        console.log(player1.attack(player2));
         if(player2.HP<=0){
             break;
         }
        // player1.HP -= player2.AP;
-        result += player2.attack(player1);
+        console.log(player2.attack(player1));
     }
     if(player1.HP<=0){
-        result+=player1.name+"被打败了";
+        console.log(player1.name+"被打败了.");
     }else{
-        result+=player2.name+"被打败了";
+        console.log(player2.name+"被打败了.");
     }
-    console.log(result);
+
 }
 
 
