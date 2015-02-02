6dca74909416e393d4416036040c71aaaeffa9b6
wfsovereign <never_fall@sohu.com>
2015-02-02 11:02:41 +0800

    use weapon specific create dizzy effect and make text success

--- src/Weapon/Specific/Dizzy_effect.js
+++ src/Weapon/Specific/Dizzy_effect.js
@@ -2,6 +2,15 @@
  * Created by wfsovereign on 15-1-21.
  */
 
+var WeaponSpecific = require('../../weapon_specific.js');
+var dizzy_effect = new WeaponSpecific(2,0,2,"击晕伤害","晕倒了","delayed_harm");
+dizzy_effect.before_attack_description = function (){
+    var result = '';
+    if(this.duration >= 0 ){
+        result += "晕倒了，无法攻击，眩晕还剩：" + this.duration + "轮\n"
+    }
+    return result
+};
 
 var Dizzy_effect = {
     effective_time:2,
@@ -20,6 +29,6 @@ var Dizzy_effect = {
     property:"delayed_harm"
 };
 
-module.exports = Dizzy_effect;
+module.exports = dizzy_effect;
 
 
