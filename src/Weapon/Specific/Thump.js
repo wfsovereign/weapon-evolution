baffad83edc2e2ab8785a27b0b0d05769c6f2b5d
wfsovereign <never_fall@sohu.com>
2015-02-02 11:12:17 +0800

    Thump of weapon specific

--- src/Weapon/Specific/Thump.js
+++ src/Weapon/Specific/Thump.js
@@ -2,7 +2,14 @@
  * Created by wfsovereign on 15-1-24.
  */
 
-var Thump = {
+var WeaponSpecific = require('../../weapon_specific.js');
+
+var Thump = new WeaponSpecific(1,0,1,"暴击","发动了全力一击,","instantaneous_harm");
+Thump.before_attack_description = function (){
+    return ''
+};
+
+/*var Thump = {
     effective_time:1,
     damage_value:0,
     duration:1,
@@ -12,7 +19,7 @@ var Thump = {
         return ""
     },
     property:"instantaneous_harm"
-};
+};*/
 
 module.exports = Thump;
 
