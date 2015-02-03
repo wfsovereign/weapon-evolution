2a184f18d7c998a770418ba0c0c4394661c8a66a
wfsovereign <never_fall@sohu.com>
2015-02-03 09:58:55 +0800

    use class of Weapon create dizzy hammer

--- src/Weapon/Specific/Toxicity_harm.js
+++ src/Weapon/Specific/Toxicity_harm.js
@@ -9,18 +9,6 @@ Toxicity_harm.before_attack_description = function(){
     return "受到" + this.damage_value + "点" + this.damage_type + ","
 };
 
-/*var Toxicity_harm = {
-    effective_time:2,
-    damage_value:2,
-    duration:2,
-    damage_type:"毒性伤害",
-    attacking_description:"中毒了",
-    before_attack_description:function(){
-      return "受到" + this.damage_value + "点" + this.damage_type + ","
-    },
-    property:"delayed_harm"
-};*/
-
 module.exports = Toxicity_harm;
 
 
