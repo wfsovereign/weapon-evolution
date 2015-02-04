c4fdd3e37d25a23a6bb76c4cead7ac3e260d8abd
wfsovereign <never_fall@sohu.com>
2015-02-04 14:26:51 +0800

    take the basic function to Player from Ordinary

--- src/Ordinary.js
+++ src/Ordinary.js
@@ -19,22 +19,5 @@ Ordinary.prototype.get_career = function () {
 };
 
 
-Ordinary.prototype.get_be_attack_point_damage = function (AP) {
-    return AP
-};
-Ordinary.prototype.get_string_of_use_attack_mode = function () {
-    return ''
-};
-
-Ordinary.prototype.get_string_of_weapon_harm_specific = function () {
-    return ''
-};
-
-Ordinary.prototype.get_string_of_attack_process = function (player2) {
-    var attack_multiple = 1;
-    this.get_be_attack_HP(player2, attack_multiple);
-    return  player2.name + "受到了" + player2.get_be_attack_point_damage(this.get_AP()) + "点伤害,"
-};
-
 module.exports = Ordinary;
 
--- src/Player.js
+++ src/Player.js
@@ -33,6 +33,24 @@ Player.prototype.get_AP = function () {
     return this.AP
 };
 
+Player.prototype.get_be_attack_point_damage = function (ap) {
+    return ap
+};
+
+Player.prototype.get_string_of_use_attack_mode = function () {
+    return ''
+};
+
+Player.prototype.get_string_of_weapon_harm_specific = function () {
+    return ''
+};
+
+Player.prototype.get_string_of_attack_process = function (player2) {
+    var attack_multiple = 1;
+    this.get_be_attack_HP(player2, attack_multiple);
+    return  player2.name + "受到了" + player2.get_be_attack_point_damage(this.get_AP()) + "点伤害,"
+};
+
 Player.prototype.get_be_attack_HP = function (player, attack_multiple) {
     player.HP -= player.get_be_attack_point_damage(this.get_AP()) * attack_multiple;
 };
